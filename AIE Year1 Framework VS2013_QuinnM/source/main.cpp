78e0ea6b682a26c606baa6884eb851c7c6905560
QuinnJMosher <Darkstaro59@gmail.com>
2015-02-24 09:29:37 -0800

    upload framework

---
+++ AIE Year1 Framework VS2013_QuinnM/source/main.cpp
@@ -0,0 +1,23 @@
+﻿#include "AIE.h"
+#include <iostream>
+
+//constant vars
+static int SCREEN_MAX_X = 900, SCREEN_MAX_Y = 600;
+
+int main( int argc, char* argv[] )
+{	
+	Initialise(SCREEN_MAX_X, SCREEN_MAX_Y, false, "My Awesome Game");
+    
+    SetBackgroundColour(SColour(0, 0, 0, 255));
+
+    //Game Loop
+    do
+	{
+        ClearScreen();
+
+    } while(!FrameworkUpdate());
+
+    Shutdown();
+
+    return 0;
+}
